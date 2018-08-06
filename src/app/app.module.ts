2b644e140144c20b3f3406a4e810e7044763b3d2
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-08-06 18:24:18 +0200

    Create add tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    AddTasksComponent
   ],
   imports: [
     BrowserModule
